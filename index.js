798ff31915751565ba4e77509776bc0cb27a2ba3
jagriti564 <singhjagriti2004@gmail.com>
2024-09-17 17:48:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// var x=4;
+// var y=5;
+// var z=x+y;
+// console.log(z)
+
+// let x=4;
+// let y=5;
+// let z=x+y;
+// console.log(z)
+
+// const x=4;
+// const y=5;
+// const z=(x+y);
+// console.log(z)
+
+// let a=6;
+// let b=5;
+// let c=8;
+// let d=(a+b+c);
+// console.log(d)
+
+// const a=6;
+// const b=5;
+// const c=8;
+// const d=(a+b+c);
+// console.log(d);
